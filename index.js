70f72af769feb4b7766e4888f0eb6d7830ef3b14
Tina Wang <mwow@yopmail.com>
2020-08-12 22:52:28 +0200

    🐛 add .js to imported files

--- index.js
+++ index.js
@@ -5,8 +5,8 @@ app.listen(3000, () => {
   console.log("serv on 3k")
 })
 
-import root_cmd from './roots/cmd/index'
+import root_cmd from './roots/cmd/index.js'
 root_cmd(app);
-import root_mongo from './roots/mongo/index'
+import root_mongo from './roots/mongo/index.js'
 root_mongo(app);
 
